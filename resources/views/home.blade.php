4eb72782ad4a8bbb96fc8d8721c25d4852852ff5
sushmikc <sushmitakhadka98@gmail.com>
2020-04-23 18:31:37 -0500

    added search box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-3">
+            <div class = "add-group">
+                <button type="button" class="btn btn-block btn-sm btn-primary">Create Group</button> 
+            </div>
+            <form class="row my-2 px-3">
+                <input class="col-9 form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="col-3 btn btn-outline-primary" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+        </div>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -13,11 +25,32 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    <!--
                     You are logged in as {{ $userName }}!
+                    -->
+                    Hi {{ $userName }}!
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+<div class="container">
+    <div class= "row no-gutters">
+        <div class= "col-md-4">
+            
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            
+        </div>
+    </div>
+</div>
+<style>
+
+</style>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" data-auto-replace-svg="nest"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
